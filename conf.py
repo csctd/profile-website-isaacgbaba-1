5858539adcd4a6e94b15bb5a039646ab9b36b9a9
isaacgbaba <110928803+isaacgbaba@users.noreply.github.com>
2023-08-04 08:44:12 -0400

    Removed reveal js

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Profile Website'
+copyright = '2023, Isaac Gbaba'
+author = 'Isaac Gbaba '
 
 
 
@@ -21,7 +21,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     "sphinx_design",
     "sphinxext.opengraph",
-    "sphinx_revealjs",
 ]
 
 # "sphinxext.rediraffe",
